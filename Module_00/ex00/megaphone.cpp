366361e2cd226797540045d5e95908bb4f094848
aoumad <abderazzakoumad@gmail.com>
2022-10-05 12:26:49 +0100

    finishing ex00 with tests success

---
+++ Module_00/ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        std::string str = std::string(argv[i]);
+        for (size_t l = 0; l < str.size(); l++)
+            std::cout << (char)std::toupper(str[l]);
+    }
+    std::cout << std::endl;
+    return (0);
+}
