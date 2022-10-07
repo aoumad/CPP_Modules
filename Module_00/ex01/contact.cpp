82da484c16c076ecda1129de11015a165f9ddfe5
aoumad <abderazzakoumad@gmail.com>
2022-10-07 16:59:31 +0100

    update

--- Module_00/ex01/contact.cpp
+++ Module_00/ex01/contact.cpp
@@ -1,55 +1,119 @@
 #include "contact.hpp"
 
+Contact::Contact()
+{
+    std::cout << "Constructor has been called" << std::endl
+}
+
+Contact::~Contact()
+{
+    std::cout << "Destructor has been called" << std::endl;
+}
+
 // GETERS
 
 std::string Contact::get_first_name()
 {
-    return (this->_first_name);
+    return (_first_name);
 }
 
 std::string Contact::get_last_name()
 {
-    return (this->_last_name);
+    return (_last_name);
 }
 
 std::string Contact::get_nickename()
 {
-    return (this->_nickename);
-}
-
-std::string Contact::get_darkest_secret()
-{
-    return (this->_darkest_secret);
+    return (_nickename);
 }
 
 std::string Contact::get_phone_number()
 {
-    return (this->_phone_number);
+    return (_phone_number);
 }
 
-// SETTERS
-
-void    Contact::set_first_name(std::string first_name)
+std::string Contact::get_darkest_secret()
 {
+    return (_darkest_secret);
+}
+
+
+int Contact::set_first_name(std::string first_name)
+{
+    if (trim(first_name) == "")
+        reutrn (1);
+    for (size_t i = 0; i < first_name.size(); i++)
+        if (!(isalpha(first_name[i]) || first_name[i] == ' '))
+            return (1);
+    if (first_name.length() > 10)
+    {
+        first_name[9] = '.';
+        first_name.erase(10, first_name.length() - 10);
+    }
     this->_first_name = first_name;
+    return (0);
 }
 
-void    Contact::set_last_name(std::string last_name)
+int Contact::set_last_name(std::string last_name)
 {
+    if (trim(last_name) == "")
+        reutrn (1);
+    for (size_t i = 0; i < last_name.size(); i++)
+        if (!(isalpha(last_name[i]) || last_name[i] == ' '))
+            return (1);
+    if (last_name.length() > 10)
+    {
+        last_name[9] = '.';
+        last_name.erase(10, last_name.length() - 10);
+    }
     this->_last_name = last_name;
 }
 
-void    Contact::set_nickename(std::string nickename)
+int Contact::set_nickename(std::string nickename)
 {
+    if (trim(nickename) == "")
+        reutrn (1);
+    for (size_t i = 0; i < nickename.size(); i++)
+        if (!(isalpha(nickename[i]) || nickename[i] == ' '))
+            return (1);
+    if (nickename.length() > 10)
+    {
+        nickename[9] = '.';
+        nickename.erase(10, nickename.length() - 10);
+    }
     this->_nickename = nickname;
 }
 
-void    Contact::set_darkest_secret(std::string darkest_secret)
+int Contact::set_phone_number(std::string phone_number)
 {
+    if (trim(phone_number) == "")
+        reutrn (1);
+    phone_number.erase(0, phone_number.find_first_not_of('0'));
+    if (phone_number == "")
+        return (1);
+    for (size_t i = 0; i < phone_number.size(); i++)
+        if (!(isalnum(phone_number[i]) || phone_number[i] == ' '))
+            return (1);
+    if (phone_number.length() > 10)
+    {
+        phone_number[9] = '.';
+        phone_number.erase(10, phone_number.length() - 10);
+    }
+    this->_phone_number = phone_number;
+}
+
+int Contact::set_darkest_secret(std::string darkest_secret)
+{
+    if (trim(first_name) == "")
+        reutrn (1);
+    for (size_t i = 0; i < darkest_secret.size(); i++)
+        if (!(isalpha(darkest_secret[i]) || darkest_secret[i] == ' '))
+            return (1);
+    if (darkest_secret.length() > 10)
+    {
+        darkest_secre[9] = '.';
+        darkest_secre.erase(10, darkest_secre.length() - 10);
+    }
     this->_darkest_secret = darkest_secret;
 }
 
-void    Contact::set_phone_number(std::string phone_number)
-{
-    this->_phone_number = phone_number;
-}
--- Module_00/ex01/contact.hpp
+++ Module_00/ex01/contact.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cstdlib>
 
 class Contact
 {
@@ -14,21 +15,25 @@ private:
     std::string _darkest_secret;
     std::string _phone_number;
 public:
-    Contact()
-    {};
-    ~Contact()
-    {};
+    Contact(
+            std::string _first_name;
+            std::string _last_name;
+            std::string _nickename;
+            std::string _darkest_secret;
+            std::string _phone_number;
+    );
+    ~Contact();
     std::string get_first_name();
     std::string get_last_name();
     std::string get_nickename();
     std::string get_darkest_secret();
     std::string get_phone_number();
 
-    void        set_first_name(std::string first_name);
-    void        set_last_name(std::string last_name);
-    void        set_nickename(std::string nickename);
-    void        set_darkest_secret(std::string darkest_secret);
-    void        set_phone_number(std::string phone_number);
+    int         set_first_name(std::string first_name);
+    int         set_last_name(std::string last_name);
+    int         set_nickename(std::string nickename);
+    int         set_darkest_secret(std::string darkest_secret);
+    int         set_phone_number(std::string phone_number);
 };
 
 #endif
