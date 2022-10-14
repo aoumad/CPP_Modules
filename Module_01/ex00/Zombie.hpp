223e98fff12079ed7d5e7a27ad48691c7232febc
aoumad <abderazzakoumad@gmail.com>
2022-10-14 23:43:08 +0100

    new class

---
+++ Module_01/ex00/Zombie.hpp
@@ -0,0 +1,22 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <string.h>
+
+class Zombie
+{
+private:
+    std::string _name;
+public:
+    Zombie();
+    ~Zombie();
+    void        announce(void);
+    void        name_setter(std::string name);
+    std::string name_getter();
+};
+
+Zombie* newZombie( std::string name );
+void randomChump( std::string name );
+
+#endif
