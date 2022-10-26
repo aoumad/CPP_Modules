ae4067c379202675f1d842cac353bea8026fd520
aoumad <abderazzakoumad@gmail.com>
2022-10-26 23:24:24 +0100

    updates

---
+++ Module_02/ex00/Fixed.cpp
@@ -0,0 +1,13 @@
+#include "Fixed.hpp"
+
+
+Class::Fixed()
+{
+    std::cout << "The constructor has been called" << std::endl;
+};
+
+Class::~Fixed()
+{
+    std::cout << "The destructor has been called" << std::endl;
+}
+
---
+++ Module_02/ex00/Fixed.hpp
@@ -0,0 +1,15 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string.h>
+
+
+class Fixed
+{
+private:
+
+public:
+    Fixed();
+    ~Fixed();
+};
